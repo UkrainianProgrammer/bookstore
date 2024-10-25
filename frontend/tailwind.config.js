4454bcf9b8656d791686e1b111ac552d829ec68f
Alex S <insten490@gmail.com>
2024-10-24 21:59:15 -0500

    updated CSS for all pages and fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,8 +10,12 @@ export default {
         "primary": "#FFCE1A",
         "secondary": "#0D0842",
         "blackBG": "#F3F3F3",
-        
-      }
+        "Favorite": "#FF5841",
+      },
+      fontFamily: {
+        "primary": ["Montserrat", "sans-serif"],
+        "secondary": ["Nunito Sans", "sans-serif"],
+      },
     },
   },
   plugins: [],
