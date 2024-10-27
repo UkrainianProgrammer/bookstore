a24bb501a6cf166ac2092e56c25be866a97f5b34
Alex S <insten490@gmail.com>
2024-10-27 16:22:10 -0500

    added left side to the navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Outlet } from 'react-router-dom'
+import { Navbar } from './components/Navbar'
 
 function App() {
 
   return (
     <>
-      <nav>Navbar</nav>
+      <Navbar />
       <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
         <Outlet />
       </main>
