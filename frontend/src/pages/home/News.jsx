245ac3f89477a5813f2cd7f9d13fbbf0e065b4f1
Alex S <insten490@gmail.com>
2024-11-25 18:26:59 -0600

    finished with the news section

--- frontend/src/pages/home/News.jsx
+++ frontend/src/pages/home/News.jsx
@@ -7,9 +7,10 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-
+import 'swiper/css/navigation';
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Pagination, Navigation } from 'swiper/modules';
+import { Link } from 'react-router-dom';
 
 const News = () => {
   return (
@@ -17,36 +18,54 @@ const News = () => {
         <h2 className='text-3xl font-semibold mb-6'>News</h2>
         <Swiper
             slidesPerView={1}
-            spaceBetween={10}
-            pagination={{
-            clickable: true,
-            }}
+            spaceBetween={30}
+            navigation={true}
             breakpoints={{
             640: {
-                slidesPerView: 2,
+                slidesPerView: 1,
                 spaceBetween: 20,
             },
             768: {
-                slidesPerView: 4,
+                slidesPerView: 2,
                 spaceBetween: 40,
             },
             1024: {
-                slidesPerView: 5,
+                slidesPerView: 2,
                 spaceBetween: 50,
             },
             }}
-            modules={[Pagination]}
+            modules={[Pagination, Navigation]}
             className="mySwiper"
         >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
-            <SwiperSlide>Slide 5</SwiperSlide>
-            <SwiperSlide>Slide 6</SwiperSlide>
-            <SwiperSlide>Slide 7</SwiperSlide>
-            <SwiperSlide>Slide 8</SwiperSlide>
-            <SwiperSlide>Slide 9</SwiperSlide>
+            {
+            news.map((item, index) => (
+                    <SwiperSlide key={index}>
+                        <div className='flex flex-col sm:flex-row sm:justify-between items-center
+                            gap-12'>
+                            <div className='py-4'>
+                                <Link
+                                    to='/'>
+                                    <h3 className='text-lg font-medium hover:text-blue-500 mb-4'>
+                                        {item.title}
+                                    </h3>
+                                </Link>
+                                <div className='w-10 h-[4px] bg-primary mb-5'></div>
+                                <p className='text-sm text-gray-600'>
+                                    {item.description}
+                                </p>
+                            </div>
+
+                            <div className='flex-shrink-0'>
+                                <img
+                                    src={item.image}
+                                    alt={item.title}
+                                    className='w-full object-cover'
+                                />
+                            </div>
+                        </div>
+                    </SwiperSlide>
+                ))
+            }
         </Swiper>
     </div>
   )
