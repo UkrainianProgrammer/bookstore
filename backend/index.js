cb635ebb0fc0015d9f0e0c89317c4141eb68cf64
Alex S <insten490@gmail.com>
2024-12-03 15:50:04 -0600

    added mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Book Server is running')
-})
+const mongoose = require('mongoose');
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  app.get('/', (req, res) => {
+    res.send('Book Server is running')
+  });
+}
+
+main().then(() => console.log("Mongodb connect successful")).catch(err => console.log(err));
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
