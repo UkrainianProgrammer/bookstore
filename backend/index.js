8b63fc8b4745aeab365953bde295d1ccda17422f
Alex S <insten490@gmail.com>
2024-12-06 21:13:29 -0600

    updated book routes; installed cors

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3000
 
 require('dotenv').config();
 const mongoose = require('mongoose');
+const bookRoutes = require('./src/books/book.route');
+
+// middleware
+app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173/'],
+    credentials: true,
+}));
+
+// routes
+app.use("/api/books", bookRoutes);
 
 async function main() {
   await mongoose.connect(process.env.DB_URL);
