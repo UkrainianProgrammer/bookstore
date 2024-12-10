6c811d4f03fa5b494393ac28f4eface1e2435ec7
Alex S <insten490@gmail.com>
2024-12-10 14:54:24 -0600

    added some messages to return api call

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const Book = require('./book.model');
 const router = express.Router();
 
 // post a book
 router.post('/create-book', async (req, res) => {
-    console.log(req.body);
+    try {
+        const newBook = await Book({...req.body});
+        await newBook.save();
+       res.status(200).send({message: 'Book created successfully', book: newBook});
+    } catch (error) {
+        console.error("Error creating book", error);
+        res.status(500).send({message: 'Failed to create book'});
+    }
 });
 
 module.exports = router;
