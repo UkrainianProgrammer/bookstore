2b19985cad985a7bcc8b8c3dbc08433bc2abe2f2
Alex S <insten490@gmail.com>
2024-12-11 10:43:19 -0600

    moved callback to controller

---
+++ backend/src/books/book.controller.js
@@ -0,0 +1,14 @@
+const Book = require("./book.model");
+
+const postABook = async (req, res) => {
+    try {
+        const newBook = await Book({...req.body});
+        await newBook.save();
+       res.status(200).send({message: 'Book created successfully', book: newBook});
+    } catch (error) {
+        console.error("Error creating book", error);
+        res.status(500).send({message: 'Failed to create book'});
+    }
+};
+
+module.exports = { postABook };
--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const Book = require('./book.model');
+const { postABook } = require('./book.controller');
 const router = express.Router();
 
 // post a book
-router.post('/create-book', async (req, res) => {
-    try {
-        const newBook = await Book({...req.body});
-        await newBook.save();
-       res.status(200).send({message: 'Book created successfully', book: newBook});
-    } catch (error) {
-        console.error("Error creating book", error);
-        res.status(500).send({message: 'Failed to create book'});
-    }
-});
+router.post('/create-book', postABook);
 
 module.exports = router;
