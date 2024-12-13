9ddab38e500c13c0314d1c26cb8e7dbd76de4fcc
Alex S <insten490@gmail.com>
2024-12-13 14:45:54 -0600

    added RTK query for redux

---
+++ frontend/src/redux/features/books/booksApi.js
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import getBaseUrl from '../../../utils/baseUrl';
+
+const baseQuery = fetchBaseQuery({
+    baseUrl: `${getBaseUrl()}/api/books`,
+    credentials: 'include',
+    prepareHeaders: (Headers) => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            Headers.set('Authorization', `Bearer ${token}`);
+        }
+        return Headers;
+    }
+})
+
+const bookApi = createApi({
+    reducerPath: 'bookApi',
+    baseQuery
+})
